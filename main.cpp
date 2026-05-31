659926ddc6e5c4cb5edc836d75174feaa0dbbc8a
youssef-joe <joe92228@gmail.com>
2026-05-31 04:38:23 +0300

    completing the algo

--- main.cpp
+++ main.cpp
@@ -22,3 +22,36 @@ bool helper(list<int> *graph, int u, bool* visited, bool* recStack) {
     recStack[u] = false;
     return false;
 }
+
+ /* the wrapper function calls helper function on each 
+ vertices which have not been visited. Helper function returns 
+ true if it detects a back edge in the subgraph(tree) or false */ 
+
+ bool isCyclic(list<int> *graph, int v) {
+    bool visited[v];
+    bool recStack[v];
+
+    for (int i = 0; i < v; ++i) 
+        visited[i] = false, recStack[i] = false; // intializing all vertices as not visited and not recursed
+
+    for (int u=0; u<v; u++) {
+        if (visited[u] == false){
+            if (helper(graph, u, visited, recStack)) // checks if the DFS tree from the vertex contains a cycle 
+                return true;
+        }
+    }
+    return false;
+    
+ }
+
+ int main() {
+    list<int>* graph = new list<int>[NUM_V];
+    graph[0].push_back(1);
+    graph[0].push_back(2);
+    graph[1].push_back(2);
+    graph[2].push_back(0);
+    graph[2].push_back(3);
+    graph[3].push_back(3);
+    bool res = isCyclic(graph, NUM_V);
+    cout << res << endl;
+ }
